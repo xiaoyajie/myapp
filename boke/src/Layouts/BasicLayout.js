ca75fbf290802c7b482a4018818e8252d5bdc607
xiaoyajie <33444638+xiaoyajie@users.noreply.github.com>
2017-12-24 17:22:01 +0800

    blog

--- boke/src/Layouts/BasicLayout.js
+++ boke/src/Layouts/BasicLayout.js
@@ -34,17 +34,31 @@ const query = {
 
 class BasicLayout extends React.Component {
 
+  constructor(props) {
+    super();
+
+    this.state = {
+      windowHeight: 0,
+      windowWidth: 0
+    }
+  }
+  componentWillMount() {
+    this.setState({
+      windowWidth: document.querySelector('body').offsetWidth,
+      windowHeight: document.querySelector('body').offsetHeight
+    })
+  }
+
   render() {
 const layout = (
 <div>
   <div style={{display:'flex', flexDirection:'row'}}>
-    <div>
-      <img src="http://demo.cssmoban.com/cssthemes4/tops_36_rabbit/assets/images/home.jpg" alt="这是一张图片"/>
+    <div  className={styles.left}>
+      <img src="http://demo.cssmoban.com/cssthemes4/tops_36_rabbit/assets/images/home.jpg" alt="这是一张图片" style={{ margin: '10px', width: '100%', height: this.state.windowHeight}} />
     </div>
-    <div >
+    <div className = {styles.right} style={{ width: '100%', height: '200px' }}>
       <img style={{margin:'100px 400px 10px 400px' }} src="http://demo.cssmoban.com/cssthemes4/tops_36_rabbit/assets/images/logo.png" alt="这是一张图片" />
       <h1 style={{margin:'0px 298px'}}> 我就是我，吃货的我</h1>
-      {/* <h2 style={{margin:'30px 270px'}}>瞅啥瞅，啥也没有</h2> */}
       <Button style={{width:'150px', height:'45px', margin:'100px 400px 20px 400px'}}><Link to="/a">我 的</Link></Button>
       <Button style={{width:'150px', height:'45px', margin:'0px 400px 20px 400px'}}><Link to="/b">日 迹</Link></Button>
       <Button style={{width:'150px', height:'45px', margin:'0px 400px 100px 400px'}}><Link to="/c">我 的 生 活</Link></Button>
